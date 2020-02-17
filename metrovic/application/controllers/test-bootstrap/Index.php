284f99501121642f8718024fce1a3e2f5e2a9dbc
bijancot <panjidia995@gmail.com>
2020-02-17 09:22:49 +0700

    add dashboard

--- metrovic/application/controllers/test-bootstrap/Index.php
+++ metrovic/application/controllers/test-bootstrap/Index.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Custom extends CI_Controller {
+class Index extends CI_Controller {
 
     public function __construct(){
        parent::__construct(); 
@@ -8,11 +8,15 @@ class Custom extends CI_Controller {
     }
     public function index(){
         // parent::__construct();
-        $data = array();
-        $data['page_title'] = 'Dashboard';
-        $data['count'] = $this->common_model->get_user_total();
-        $data['main_content'] = $this->load->view('admin/home', $data, TRUE);
-        $this->load->view('admin/index',$data);
+        // $data = array();
+        // $data['page_title'] = 'Dashboard';
+        // $data['count'] = $this->common_model->get_user_total();
+        // $data['main_content'] = $this->load->view('admin/home', $data, TRUE);
+        // $this->load->view('admin/index',$data);
+        $this->load->view('template/header');
+        $this->load->view('template/sidebar');
+        $this->load->view('template/main-test');
+        $this->load->view('template/footer');
     }
 
     public function test(){
