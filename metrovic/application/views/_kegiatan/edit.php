f9883f0edf5fd61497c0da5f3d8707716ebe221a
irfansyarifuddin19 <irfansyarifuddin@gmail.com>
2020-02-21 19:28:19 +0700

    Added Views for _kegiatan

---
+++ metrovic/application/views/_kegiatan/edit.php
@@ -0,0 +1,122 @@
+
+
+<!-- Container fluid  -->
+
+<div class="container-fluid">
+    
+    <!-- Bread crumb and right sidebar toggle -->
+    
+    <div class="row page-titles">
+        <div class="col-md-5 col-8 align-self-center">
+            <h3 class="text-themecolor m-b-0 m-t-0">Home</h3>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="javascript:void(0)">Kegiatan</a></li>
+                <li class="breadcrumb-item active">Edit Kegiatan</li>
+            </ol>
+        </div>
+        <div class="col-md-7 col-4 align-self-center">
+            
+            
+            <div class="d-flex m-t-10 justify-content-end">
+                <div class="">
+                    <button class="right-side-toggle waves-effect waves-light btn-success btn btn-circle btn-sm pull-right m-l-10"><i class="ti-settings text-white"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- End Bread crumb and right sidebar toggle -->
+    
+
+    
+    <!-- Start Page Content -->
+
+    <div class="row">
+        <div class="col-12">
+
+            <?php $msg = $this->session->flashdata('msg'); ?>
+            <?php if (isset($msg)): ?>
+                <div class="alert alert-success delete_msg pull" style="width: 100%"> <i class="fa fa-check-circle"></i> <?php echo $msg; ?> &nbsp;
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button>
+                </div>
+            <?php endif ?>
+
+            <?php $error_msg = $this->session->flashdata('error_msg'); ?>
+            <?php if (isset($error_msg)): ?>
+                <div class="alert alert-danger delete_msg pull" style="width: 100%"> <i class="fa fa-times"></i> <?php echo $error_msg; ?> &nbsp;
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button>
+                </div>
+            <?php endif ?>
+
+            <div class="card">
+
+                <div class="card-body">
+
+                <?php if ($this->session->userdata('role') == 'admin'): ?>
+                    <a href="<?php echo base_url('admin/user') ?>" class="btn btn-info pull-right"><i class="fa fa-plus"></i> Add New User</a> &nbsp;
+
+                    <a href="<?php echo base_url('admin/user/power') ?>" class="btn btn-info"><i class="fa fa-user-o"></i> &nbsp; Add User Power</a>
+                <?php else: ?>
+                    <!-- check logged user role permissions -->
+
+                    <?php if(check_power(1)):?>
+                        <a href="<?php echo base_url('admin/user') ?>" class="btn btn-info pull-right"><i class="fa fa-plus"></i> Add New User</a>
+                    <?php endif; ?>
+                <?php endif ?>
+                
+
+                    <div class="table-responsive m-t-40">
+					<?php echo form_open('_kegiatan/edit/'.$_kegiatan['id_kegiatan'],array("class"=>"form-horizontal")); ?>
+
+	<div class="form-group">
+		<label for="id_program" class="col-md-4 control-label"> Program</label>
+		<div class="col-md-8">
+			<select name="id_program" class="form-control">
+				<option value="">select _program</option>
+				<?php 
+				foreach($all__program as $_program)
+				{
+					$selected = ($_program['id_program'] == $_kegiatan['id_program']) ? ' selected="selected"' : "";
+
+					echo '<option value="'.$_program['id_program'].'" '.$selected.'>'.$_program['id_program'].'</option>';
+				} 
+				?>
+			</select>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="kd_keg" class="col-md-4 control-label">Kd Keg</label>
+		<div class="col-md-8">
+			<input type="text" name="kd_keg" value="<?php echo ($this->input->post('kd_keg') ? $this->input->post('kd_keg') : $_kegiatan['kd_keg']); ?>" class="form-control" id="kd_keg" />
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="program" class="col-md-4 control-label">Program</label>
+		<div class="col-md-8">
+			<input type="text" name="program" value="<?php echo ($this->input->post('program') ? $this->input->post('program') : $_kegiatan['program']); ?>" class="form-control" id="program" />
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="kegiatan" class="col-md-4 control-label">Kegiatan</label>
+		<div class="col-md-8">
+			<input type="text" name="kegiatan" value="<?php echo ($this->input->post('kegiatan') ? $this->input->post('kegiatan') : $_kegiatan['kegiatan']); ?>" class="form-control" id="kegiatan" />
+		</div>
+	</div>
+	
+	<div class="form-group">
+		<div class="col-sm-offset-4 col-sm-8">
+			<button type="submit" class="btn btn-success">Save</button>
+        </div>
+	</div>
+	
+<?php echo form_close(); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- End Page Content -->
+
+</div>
